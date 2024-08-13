8cba4ea494f3a50744e34e72bde415d706fee975
manasvakani <140334953+manasvakani@users.noreply.github.com>
2024-08-13 19:07:35 +0400

    Update page.tsx

--- app/(students)/studetails/page.tsx
+++ app/(students)/studetails/page.tsx
@@ -25,58 +25,12 @@ const LoginPage = () => {
     "image/png",
     "image/webp",
   ];
-  const UserSchema = z.object({
-    fname: z.string().min(1, { message: "First name is required." }),
-    lname: z.string().min(1, { message: "Last name is required." }),
-    gender: z.enum(["male", "female"], {
-      message: "Gender is required",
-    }),
-    grade: z.enum(
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-      {
-        message: "Grade is required",
-      }
-    ),
-    f_fname: z.string().min(1, { message: "Father's first name is required" }),
-    m_fname: z
-      .string()
-      .min(10)
-      .regex(/^((\+971)|0)?5[024568][0-9]{7}$/, "Invalid UAE phone number"),
-    email: z.string().email().min(1),
-    image: z
-      .any()
-      .refine((files) => files?.length == 1, "Image is required.")
-      .refine(
-        (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-        `Max file size is 5MB.`
-      )
-      .refine(
-        (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-        ".jpg, .jpeg, .png and .webp files are accepted."
-      ),
-  });
-
-  type User = z.infer<typeof UserSchema>;
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setValue,
-    formState: { errors },
-  } = useForm<User>({ resolver: zodResolver(UserSchema), mode: "onTouched" });
-  const onSubmit: SubmitHandler<User> = (data) => {
-    console.log("its working");
-    console.log(data);
-    router.push("/studesc");
-  };
-  const [selectedOption, setSelectedOption] = useState("apple");
 
   return (
     <div className="w-screen h-screen grid place-content-center">
-      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="container register-box w-full h-auto grid grid-rows-8 gap-5 place-content-center ">
           <div className="title text-white font-bold text-[3rem] w-full grid justify-start row-span-1 h-auto place-content-end">
-            Add a student
+            Student Details
           </div>
           <div className=" grid grid-cols-2 gap-5 w-full place-content-start row-span-1 h-auto">
             <div className="fname col-span-1 w-[22.5rem]">
@@ -86,14 +40,15 @@ const LoginPage = () => {
               >
                 Student's First name
               </label>
-              <Input
-                type="text"
-                id="fname"
-                {...register("fname", { required: true })}
-                className="  py-3 rounded-sm col-span-full h-[3rem] focus:outline-slate-800 w-full"
-                placeholder="Enter your full name"
-              />
-              <p className="text-red-500 pl-2">{errors.fname?.message}</p>
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
+              >
+                First name
+              </label>
+              
+              
+             
             </div>
 
             <div className="phone col-span-1 ">
@@ -103,14 +58,13 @@ const LoginPage = () => {
               >
                 Student's Last Name
               </label>
-              <Input
-                type="text"
-                id="lname"
-                {...register("lname", { required: true })}
-                className="  py-3 rounded-sm col-span-full h-[3rem] focus:outline-slate-800 w-full"
-                placeholder="Enter your Last name"
-              />
-              <p className="text-red-500 pl-2">{errors.lname?.message}</p>
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
+              >
+                Last name
+              </label>
+              
             </div>
           </div>
           <div className=" grid grid-cols-2 gap-5 w-full place-content-start row-span-1 h-auto">
@@ -121,28 +75,14 @@ const LoginPage = () => {
               >
                 Gender
               </label>
-              <Select
-                onValueChange={(value) => {
-                  console.log(value);
-                  setValue("gender", value, {
-                    shouldValidate: true,
-                  });
-                }}
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
               >
-                <SelectTrigger
-                  className="w-full"
-                  id="gender"
-                  {...register("gender", { required: true })}
-                >
-                  <SelectValue placeholder="Select a Gender" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="male">Male</SelectItem>
-                  <SelectItem value="female">Female</SelectItem>
-                </SelectContent>
-              </Select>
+                Gender
+              </label>
 
-              <p className="text-red-500 pl-2">{errors.gender?.message}</p>
+              
             </div>
 
             <div className="phone col-span-1 ">
@@ -152,37 +92,13 @@ const LoginPage = () => {
               >
                 Grade
               </label>
-              <Select
-                onValueChange={(value) => {
-                  console.log(value);
-                  setValue("grade", value, {
-                    shouldValidate: true,
-                  });
-                }}
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
               >
-                <SelectTrigger
-                  className="w-full"
-                  id="grade"
-                  {...register("grade", { required: true })}
-                >
-                  <SelectValue placeholder="Select a Grade" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">1</SelectItem>
-                  <SelectItem value="2">2</SelectItem>
-                  <SelectItem value="3">3</SelectItem>
-                  <SelectItem value="4">4</SelectItem>
-                  <SelectItem value="5">5</SelectItem>
-                  <SelectItem value="6">6</SelectItem>
-                  <SelectItem value="7">7</SelectItem>
-                  <SelectItem value="8">8</SelectItem>
-                  <SelectItem value="9">9</SelectItem>
-                  <SelectItem value="10">10</SelectItem>
-                  <SelectItem value="11">11</SelectItem>
-                  <SelectItem value="12">12</SelectItem>
-                </SelectContent>
-              </Select>
-              <p className="text-red-500 pl-2">{errors.grade?.message}</p>
+                Grade
+              </label>
+              
             </div>
           </div>
           <div className=" grid grid-cols-2 gap-5 w-full place-content-start row-span-1 h-auto">
@@ -193,14 +109,13 @@ const LoginPage = () => {
               >
                 Parent's Full name
               </label>
-              <Input
-                type="text"
-                id="f_fname"
-                {...register("f_fname", { required: true })}
-                className="  py-3 rounded-sm col-span-full h-[3rem] focus:outline-slate-800 w-full"
-                placeholder="Enter the Father's full name"
-              />
-              <p className="text-red-500 pl-2">{errors.f_fname?.message}</p>
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
+              >
+                Parent Full name
+              </label>
+              
             </div>
 
             <div className="phone col-span-1 ">
@@ -210,14 +125,13 @@ const LoginPage = () => {
               >
                 Parent's phone number
               </label>
-              <Input
-                type="text"
-                id="lname"
-                {...register("m_fname", { required: true })}
-                className="  py-3 rounded-sm col-span-full h-[3rem] focus:outline-slate-800 w-full"
-                placeholder="Enter the Parent's phone number"
-              />
-              <p className="text-red-500 pl-2">{errors.m_fname?.message}</p>
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
+              >
+                Parent's phone number
+              </label>
+              
             </div>
           </div>
           <div className=" grid grid-cols-2 gap-5 place-content-start row-span-1 h-auto">
@@ -228,31 +142,19 @@ const LoginPage = () => {
               >
                 Parent's Email
               </label>
-              <Input
-                type="text"
-                id="email"
-                {...register("email", { required: true })}
-                className="  py-3 rounded-sm col-span-full h-[3rem] focus:outline-slate-800 w-full"
-                placeholder="Enter the Parent's Email ID"
-              />
-              <p className="text-red-500 pl-2">{errors.email?.message}</p>
+              <br/>
+              <label
+                className="text-white focus:outline-slate-800 font-[100] py-3 rounded-sm col-span-full h-[3rem]  w-[22.5rem]"
+              >
+                Parent's Email
+              </label>
             </div>
             <div className="grid w-full max-w-sm items-center gap-1.5">
               <label htmlFor="picture">Student's Picture</label>
-              <Input id="picture" type="file" {...register("image")} />
-              <p className="text-red-500 pl-2">{errors?.image?.message}</p>
             </div>
           </div>
 
           <div className="next-btn grid justify-center row-span-2 place-content-end">
-            <Button
-              type="submit"
-              variant="outline"
-              size="default"
-              className="py-6 px-7 mt-8 "
-            >
-              <ArrowRight size={28} />
-            </Button>
             <Button
               type="submit"
               variant="link"
@@ -267,7 +169,6 @@ const LoginPage = () => {
             </Button>
           </div>
         </div>
-      </form>
     </div>
   );
 };
